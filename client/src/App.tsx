22c7c8665239c4e31b66b68f1ef2a49aa682a8ff
maayan <maayan@gmail.com>
2024-12-14 21:40:26 +0200

    Add a skeleton for frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Heading, VStack } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div>
+      <VStack paddingTop={5}>
+        <Heading size="2xl">URL Shortener</Heading>
+      </VStack>
+    </div>
+  );
+}
+
+export default App;
