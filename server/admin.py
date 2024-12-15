63bf14ed603235d189fa730e04be7b9142eb4e78
maayan <maayan@gmail.com>
2024-12-15 18:08:11 +0200

    Customized the Admin site and fixed a minor bug

--- server/admin.py
+++ server/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+from . import models
+
+admin.site.site_header = "URL Shortener Admin"
+admin.site.index_title = "Admin"
+
+
+@admin.register(models.Url)
+class UrlAdmin(admin.ModelAdmin):
+    list_display = ["url", "shortened_url"]
+    actions = ["delete_selected_urls"]
+
+    @admin.action(description="Delete selected URLs")
+    def delete_selected_urls(self, request, queryset):
+        count = queryset.count()
+        queryset.delete()
+        self.message_user(request, f"Successfully deleted {count} URLs.")
