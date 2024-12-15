9451da102dffce2f85524cb8817af3b2e70de5ca
MaayanBah <maayanbah@gmail.com>
2024-12-15 17:36:11 +0200

    Validating the URL

--- server/views.py
+++ server/views.py
@@ -3,39 +3,40 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Url
 from django.shortcuts import get_object_or_404, redirect
+from .serializers import UrlSerializer
 
 
 @api_view(["POST"])
 def shorten_url(request):
     try:
-        url = request.data.get("url")
+        serializer = UrlSerializer(data={"url": request.data.get("url")})
 
-        if not url:
-            return Response(
-                {"error": "URL not provided"}, status=status.HTTP_400_BAD_REQUEST
-            )
+        if serializer.is_valid():
+            url = serializer.validated_data["url"]
 
-        existing_url = Url.objects.filter(url=url).first()
-        if existing_url:
-            short_url = f"http://{request.get_host()}/{existing_url.shortened_url}"
+            existing_url = Url.objects.filter(url=url).first()
+            if existing_url:
+                short_url = f"http://{request.get_host()}/{existing_url.shortened_url}"
+                return Response(
+                    {
+                        "short_url": short_url,
+                        "short_code": existing_url.shortened_url,
+                    },
+                    status=status.HTTP_200_OK,
+                )
+
+            new_url = Url.objects.create(url)
+            short_url = f"http://{request.get_host()}/{new_url.shortened_url}"
+
             return Response(
                 {
                     "short_url": short_url,
-                    "short_code": existing_url.shortened_url,
+                    "short_code": new_url.shortened_url,
                 },
                 status=status.HTTP_200_OK,
             )
-
-        new_url = Url.create(url)
-        short_url = f"http://{request.get_host()}/{new_url.shortened_url}"
-
-        return Response(
-            {
-                "short_url": short_url,
-                "short_code": new_url.shortened_url,
-            },
-            status=status.HTTP_200_OK,
-        )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
