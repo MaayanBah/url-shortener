23f5b6426c29f4d02f1e1b3b2af1914099698696
maayan <maayan@gmail.com>
2024-12-15 16:30:48 +0200

    Add error handling for URL shortening requests

--- client/src/hooks/useShorten.ts
+++ client/src/hooks/useShorten.ts
@@ -1,19 +1,28 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
+import { AxiosError } from "axios";
 
 const useShorten = (searchText: string = "") => {
   const [shortenUrl, setShortenUrl] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
   useEffect(() => {
     if (searchText) {
       const shortenUrlRequest = async () => {
-        const response = await apiClient.post("/shorten", { url: searchText });
-        setShortenUrl(response.data.short_url);
+        try {
+          const response = await apiClient.post("/shorten", {
+            url: searchText,
+          });
+          setShortenUrl(response.data.short_url);
+        } catch (err: any) {
+          const axiosError = err as AxiosError;
+          setError(axiosError.message);
+        }
       };
       shortenUrlRequest();
     }
   }, [searchText]);
 
-  return { shortenUrl };
+  return { shortenUrl, error };
 };
 
 export default useShorten;
